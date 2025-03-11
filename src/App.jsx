9dd26e050486033dfe6b0392b5112dbeb9cfb74f
Jaunty <jaunty5899@gmail.com>
2025-03-11 13:37:07 +0530

    miui added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="container">tis working</div>;
+  return (
+    <Box
+      sx={{
+        border: "1px solid grey",
+        display: "flex",
+      }}
+    >
+      <Box sx={{ p: 2, m: 0, border: "1px dashed grey" }}>
+        This Box renders as an HTML section element.
+      </Box>
+      <Box sx={{ p: 2, border: "1px dashed grey", flexGrow: 1 }}>
+        This Box renders as an HTML section element.
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
